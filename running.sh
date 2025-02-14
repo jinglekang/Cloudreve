b6d28131615eae85f20a7f7ba7d00fd7358f33bb
JINGLE <jinglekang@live.cn>
2025-02-14 15:30:33 +0800

    Update running.sh

--- running.sh
+++ running.sh
@@ -7,4 +7,4 @@ docker stop cloudreve
 # 删除老容器
 docker rm cloudreve
 # 启动服务
-docker run -d --name cloudreve -p 5200:5200 -v /root/docker/cloudreve/cloudreve.db:/cloudreve/cloudreve.db -v /root/docker/cloudreve/avatar:/cloudreve/avatar -v /root/cloudreve/uploads:/cloudreve/uploads -v /root/docker/cloudreve/data:/data --restart unless-stopped cloudreve:latest
+docker run -d --name cloudreve -p 5200:5200 -v /root/docker/cloudreve/cloudreve.db:/cloudreve/cloudreve.db -v /root/docker/cloudreve/conf.ini:/cloudreve/conf.ini -v /root/docker/cloudreve/avatar:/cloudreve/avatar -v /root/cloudreve/uploads:/cloudreve/uploads --restart unless-stopped cloudreve:latest
